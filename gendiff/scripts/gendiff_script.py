fa90f1ebbe8937736951afa8d034a624d3b07565
Alex Kositskiy <koso-96@mail.ru>
2023-06-26 19:59:12 +0300

    update gendiff-scripts.py

--- gendiff/scripts/gendiff_script.py
+++ gendiff/scripts/gendiff_script.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+import argparse
+
+
+parser = argparse.ArgumentParser(
+    prog='gendiff',
+    description='Compares two configuration files and shows a difference.'
+    )
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+parser.print_help()
+
 
 def main():
     print('Hello world!')
