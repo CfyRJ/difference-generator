c915b65dc09e6b0723b0dcdd919aebb9e6b9438b
Alex Kositskiy <koso-96@mail.ru>
2023-06-29 21:56:50 +0300

    add test_gendiff.py

---
+++ tests/test_gendiff.py
@@ -0,0 +1,19 @@
+from gendiff.gendiff import generate_diff
+
+
+P1 = 'data/file1.json'
+P2 = 'data/file2.json'
+
+
+def test_generate_diff():
+    check = '''{
+  - follow: False
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: True
+}'''
+    res = generate_diff(P1, P2)
+
+    assert res == check
