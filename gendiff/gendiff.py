0e37d731455127b313fdc9ddcb2a4f753d421b6e
Alex Kositskiy <koso-96@mail.ru>
2023-07-03 20:34:18 +0300

    update gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -46,34 +46,6 @@ def generate_diff(path_file1: str, path_file2: str) -> str:
     res = '\n'.join(res)
 
     return res
-# import json
-
-
-# def generate_diff(file1_path: str, file2_path: str) -> str:
-#     values_up_to = json.load(open(file1_path))
-#     values_after = json.load(open(file2_path))
-
-#     values = []
-
-#     for k, v in values_up_to.items():
-#         if k in values_after.keys() and values_after[k] == v:
-#             values += [['   ', k, v]]
-#             continue
-#         if k in values_after.keys() and values_after[k] != v:
-#             values += [['  -', k, v], ['  +', k, values_after[k]]]
-#             continue
-#         values += [['  -', k, v]]
-    
-#     for k, v in values_after.items():
-#         if k not in values_up_to.keys():
-#             values += [['  +', k, v]]
-    
-#     values.sort(key=lambda x: x[1])
-#     values = ['{'] + list(map(lambda x: f'{x[0]} {x[1]}: {x[2]}', values)) + ['}']
-#     values = '\n'.join(values)
-
-#     return values
-
 
 
 # p1 = 'second-project/python-project-50/data/file1.json'
