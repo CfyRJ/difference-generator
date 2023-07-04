f7f04c2af0d79ee4412209044e419b3daf6a9cb4
Alex Kositskiy <koso-96@mail.ru>
2023-07-04 21:37:57 +0300

    bugfix gendiff_script.py

--- gendiff/scripts/gendiff_script.py
+++ gendiff/scripts/gendiff_script.py
@@ -6,14 +6,14 @@ import argparse
 parser = argparse.ArgumentParser(
     prog='gendiff',
     description='Compares two configuration files and shows a difference.'
-    )
+)
 parser.add_argument('first_file')
 parser.add_argument('second_file')
 parser.add_argument(
     '-f FORMAT',
     metavar='--format FORMAT',
     help='set format of output'
-    )
+)
 parser.print_help()
 
 
