007e60319ecd6922275108867e7546130219af87
Alex Kositskiy <koso-96@mail.ru>
2023-07-08 15:10:02 +0300

    update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,17 +3,53 @@ from gendiff.gendiff import generate_diff
 
 P1 = 'tests/fixtures/file1.json'
 P2 = 'tests/fixtures/file2.json'
+CHECK = """{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}"""
 
 
 def test_generate_diff():
-    check = '''{
-  - follow: False
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: True
-}'''
     res = generate_diff(P1, P2)
 
-    assert res == check
+    assert res == CHECK
