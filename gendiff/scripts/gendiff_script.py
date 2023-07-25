843556b9ce6e95e80ce193489aab97fc9e9b4c93
Alex Kositskiy <koso-96@mail.ru>
2023-07-25 22:26:51 +0300

    update gendiff_script.py

--- gendiff/scripts/gendiff_script.py
+++ gendiff/scripts/gendiff_script.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
 
 
-# import argparse
+import argparse
 
 
-# from gendiff.gendiff import generate_diff
+from gendiff.gendiff import generate_diff
 
 
-def main(path_file1, path_file2, format='stylish'):
-    # generate_diff(path_file1, path_file2, format='stylish')
-    pass
+TEXT_HELP = '''Compares two configuration files and shows a difference.
+Available formats: stylish, plain, json.'''
+
+
+def main():
+    res = generate_diff(args.path_file1, args.path_file2, args.format)
+    print(res)
+
+
+parser = argparse.ArgumentParser(description=TEXT_HELP)
+
+parser.add_argument('path_file1', type=str, help='Path to the first (old) file.')
+parser.add_argument('path_file2', type=str, help='Path to the second (new) file.')
+parser.add_argument('-f', '--format', dest='format', default='stylish', type=str, help='Result output format.')
+
+args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    # main(path_file1, path_file2, format='stylish')
-    pass
+    main()
