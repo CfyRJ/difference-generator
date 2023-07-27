d2415f3b3d07d2165328ecae6bf28d8bedf30e3d
Alex Kositskiy <koso-96@mail.ru>
2023-07-27 17:57:47 +0300

    update gendiff_script.py

--- gendiff/scripts/gendiff_script.py
+++ gendiff/scripts/gendiff_script.py
@@ -7,7 +7,9 @@ from gendiff.gendiff import generate_diff
 
 def main():
     args = make_parser().parse_args()
-    generate_diff(args.path_file1, args.path_file2, args.format)
+    res = generate_diff(args.path_file1, args.path_file2, args.format)
+
+    print(res)
 
     return None
 
