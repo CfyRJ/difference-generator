b900f8e588e34d45e94078054851c8b69fa3473c
Alex Kositskiy <koso-96@mail.ru>
2023-08-05 15:23:52 +0300

    update plain.py

--- gendiff/format/plain.py
+++ gendiff/format/plain.py
@@ -1,12 +1,13 @@
+STATUS_VALUES = {'a': 'add', 'ch': 'changed', 'r': 'removed'}
+VALUE_ACCESS_KEYS = {'s': 'status', 'o': 'old_value', 'n': 'new_value'}
+
 VALUES_DEFAULT = {
-    'removed': ' was removed',
-    'add': ' was added with value: ',
-    'change': ' was updated. From ',
-    'change_to': ' to ',
+    STATUS_VALUES['r']: ' was removed',
+    STATUS_VALUES['a']: ' was added with value: ',
+    STATUS_VALUES['ch']: (' was updated. From ', ' to '),
     'first_word': 'Property '
 }
 CONSTANT_CHANGE = {'False': 'false', 'True': 'true', 'None': 'null', '0': '0'}
-VALUE_ACCESS_KEYS = {'s': 'status', 'o': 'old_value', 'n': 'new_value'}
 HIDDEN_DICT = '[complex value]'
 
 
@@ -17,37 +18,45 @@ def make_plain(data: dict) -> list:
         if not isinstance(values, dict):
             continue
 
-        status_keys = values.keys()
+        value_keys = values.keys()
 
         if not VALUE_ACCESS_KEYS['s'] in values.keys():
-            for v in make_plain(values):
-                res += [[f"{key}."] + v]
+            for value in make_plain(values):
+                res += [[f"{key}."] + value]
             continue
 
-        if not VALUE_ACCESS_KEYS['o'] in status_keys:
-            n = values[VALUE_ACCESS_KEYS['n']]
-            value = f"'{n}'" if not isinstance(n, dict) else HIDDEN_DICT
+        if not VALUE_ACCESS_KEYS['o'] in value_keys:
+            new_value = values[VALUE_ACCESS_KEYS['n']]
+            if isinstance(new_value, dict):
+                value = HIDDEN_DICT
+            else:
+                value = f"'{new_value}'"
 
-            res += [[key] + [VALUES_DEFAULT['add']] + [value]]
+            res += [[key]
+                    + [VALUES_DEFAULT[values[VALUE_ACCESS_KEYS['s']]]]
+                    + [value]]
             continue
 
-        if not VALUE_ACCESS_KEYS['n'] in status_keys:
-            o = values[VALUE_ACCESS_KEYS['o']]
-            value = f"'{o}'" if not isinstance(o, dict) else HIDDEN_DICT
-
-            res += [[key] + [VALUES_DEFAULT['removed']]]
+        if not VALUE_ACCESS_KEYS['n'] in value_keys:
+            res += [[key] + [VALUES_DEFAULT[values[VALUE_ACCESS_KEYS['s']]]]]
             continue
 
-        o = values[VALUE_ACCESS_KEYS['o']]
-        n = values[VALUE_ACCESS_KEYS['n']]
+        old_value = values[VALUE_ACCESS_KEYS['o']]
+        new_value = values[VALUE_ACCESS_KEYS['n']]
 
-        old_value = f"'{o}'" if not isinstance(o, dict) else HIDDEN_DICT
-        new_value = f"'{n}'" if not isinstance(n, dict) else HIDDEN_DICT
+        if isinstance(old_value, dict):
+            old_value = HIDDEN_DICT
+        else:
+            old_value = f"'{old_value}'"
+        if isinstance(new_value, dict):
+            new_value = HIDDEN_DICT
+        else:
+            new_value = f"'{new_value}'"
 
         res += [[key]
-                + [VALUES_DEFAULT['change']]
+                + [VALUES_DEFAULT[values[VALUE_ACCESS_KEYS['s']]][0]]
                 + [old_value]
-                + [VALUES_DEFAULT['change_to']]
+                + [VALUES_DEFAULT[values[VALUE_ACCESS_KEYS['s']]][1]]
                 + [new_value]]
 
     return res
@@ -55,8 +64,8 @@ def make_plain(data: dict) -> list:
 
 def plain(data: dict) -> str:
 
-    lines = make_plain(data)
-    lines = sorted([''.join(v) for v in lines])
+    processed_data = make_plain(data)
+    lines = sorted([''.join(v) for v in processed_data])
     lines = list(map(lambda x:
                      ' '.join([VALUES_DEFAULT['first_word']
                                + f"'{x.split()[0]}'"]
@@ -65,7 +74,7 @@ def plain(data: dict) -> str:
 
     res = '\n'.join(lines)
 
-    for k, v in CONSTANT_CHANGE.items():
-        res = res.replace(f"'{k}'", v)
+    for key, value in CONSTANT_CHANGE.items():
+        res = res.replace(f"'{key}'", value)
 
     return res
