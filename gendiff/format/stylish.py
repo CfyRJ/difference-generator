8edfcf48629f11b413e872611518bfb9adef5208
Alex Kositskiy <koso-96@mail.ru>
2023-08-05 13:00:56 +0300

    update stylish.py

--- gendiff/format/stylish.py
+++ gendiff/format/stylish.py
@@ -4,35 +4,89 @@ START_RES = '{'
 END_RES = '}'
 
 CONSTANT_CHANGE = {'False': 'false', 'True': 'true', 'None': 'null'}
+VALUE_ACCESS_KEYS = {'s': 'status', 'o': 'old_value', 'n': 'new_value'}
+PREFIXES = {VALUE_ACCESS_KEYS['o']: '  - ', VALUE_ACCESS_KEYS['n']: '  + ', }
 
 
-def make_data(data: list) -> str:
+def flatten(data: list) -> list:
+    '''
+    Converts a multi-nested list to a single-nested list.
+    In the original list, the nesting must be in the last element.
+    '''
     res = []
-    n, pr, k, val = data[0], data[1], data[2], data[3]
+    head, tail = data[:-1], data[-1]
 
-    if isinstance(val, list):
-        res += [f'{n*INDENT}{pr}{k}: {START_RES}']
+    if isinstance(tail, list):
+        res += [''.join(head)]
 
-        for v in val:
-            if isinstance(v, list):
-                res += make_data(v)
-
-        res += [f'{n*INDENT}{INDENT}{END_RES}']
+        for element in tail:
+            if isinstance(element, list):
+                res += flatten(element)
     else:
-        res += [f'{n*INDENT}{pr}{k}: {val}']
+        res += [''.join(data)]
 
     return res
 
 
-def stylish(data: list) -> str:
-    if not isinstance(data, list):
-        return 'Error type'
-
+def make_data(data: dict, nesting=0) -> list:
     res = []
-    for val in data:
-        res += make_data(val)
 
-    res = '\n'.join((START_RES, *res, END_RES))
+    for key, value in data.items():
+        values = ''
+        if not isinstance(value, dict):
+            res.append([nesting * INDENT,
+                        INDENT,
+                        key,
+                        f': {value}',
+                        values])
+            continue
+
+        if not value.get(VALUE_ACCESS_KEYS['s']):
+            values = make_data(value, nesting + 1)
+            res.append([nesting * INDENT,
+                        INDENT,
+                        key,
+                        f': {START_RES}',
+                        values])
+            continue
+
+        for status_key, prefix_value in PREFIXES.items():
+            values = ''
+            if status_key not in value.keys():
+                continue
+
+            status_value = value[status_key]
+
+            if isinstance(status_value, dict):
+                values = make_data(status_value, nesting + 1)
+                res.append([nesting * INDENT,
+                            prefix_value,
+                            key,
+                            f': {START_RES}',
+                            values])
+            else:
+                res.append([nesting * INDENT,
+                            prefix_value,
+                            key,
+                            f': {status_value}',
+                            values])
+
+    res.sort(key=lambda x: x[2])
+
+    res.append([nesting * INDENT,
+                END_RES])
+
+    return res
+
+
+def stylish(data: dict) -> str:
+    res = []
+    tmp_data = make_data(data)
+
+    for d in tmp_data:
+        res += flatten(d)
+
+    res = '\n'.join((START_RES, *res))
 
     for k, v in CONSTANT_CHANGE.items():
         res = res.replace(k, v)
