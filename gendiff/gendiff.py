3b96234c5e503d0b6779a1c9fe7c55c7b31c86ea
Alex Kositskiy <koso-96@mail.ru>
2023-08-05 12:57:03 +0300

    update gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,69 +1,58 @@
 from gendiff.make_data import make_value
 from gendiff.format.stylish import stylish
 from gendiff.format.plain import plain
-from gendiff.format.json_form import json_form
+from gendiff.format.json import format_json
+
+# from make_data import make_value
+# from format.stylish import stylish
+# from format.plain import plain
+# from format.json import format_json
+
+VALUE_ACCESS_KEYS = {'s': 'status', 'o': 'old_value', 'n': 'new_value'}
+FORMAT_FUNCTIONS = {'stylish': stylish, 'plain': plain, 'json': format_json}
+DEFAULT_FORMAT_FUNCTIONS = 'stylish'
 
 
-FORMAT_FUNCTIONS = {'stylish': stylish, 'plain': plain, 'json': json_form}
-
-
-PREFIX_F1 = '  - '
-PREFIX_F2 = '  + '
-
-INDENT = '    '
-
-START_RES = '{'
-END_RES = '}'
-
-CONSTANT_JSON = {'False': 'false', 'True': 'true', 'None': 'null'}
-CONSTANT_YAML = {'False': 'false', 'True': 'true', 'None': 'null'}
-
-
-def get_diff(value_old: dict, value_new: dict, nesting=0) -> list:
+def get_diff(value_old: dict, value_new: dict, nesting=0) -> dict:
     k1 = list(value_old.keys())
     k2 = list(value_new.keys())
     keys = set(k1 + k2)
 
-    values = []
+    values = {}
 
     for k in keys:
         v1 = value_old.get(k)
         v2 = value_new.get(k)
 
         if isinstance(v1, dict) and isinstance(v2, dict):
-            values.append([nesting, INDENT, k, get_diff(v1, v2, nesting + 1)])
+            values[k] = get_diff(v1, v2)
             continue
 
-        if isinstance(v1, dict):
-            v1 = get_diff(v1, v1, nesting + 1)
-        if isinstance(v2, dict):
-            v2 = get_diff(v2, v2, nesting + 1)
-
         if k in k1 and k not in k2:
-            values.append([nesting, PREFIX_F1, k, v1])
+            values[k] = {VALUE_ACCESS_KEYS['s']: 'removed',
+                         VALUE_ACCESS_KEYS['o']: v1}
             continue
 
         if k not in k1 and k in k2:
-            values.append([nesting, PREFIX_F2, k, v2])
+            values[k] = {VALUE_ACCESS_KEYS['s']: 'add',
+                         VALUE_ACCESS_KEYS['n']: v2}
             continue
 
         if v1 == v2:
-            values.append([nesting, INDENT, k, v1])
+            values[k] = v1
             continue
 
-        if isinstance(v1, dict) and isinstance(v2, dict):
-            values.append([nesting, INDENT, k, get_diff(v1, v2, nesting + 1)])
-            continue
-
-        values.append([nesting, PREFIX_F1, k, v1])
-        values.append([nesting, PREFIX_F2, k, v2])
-
-    values.sort(key=lambda x: x[2])
+        values[k] = {VALUE_ACCESS_KEYS['s']: 'changed',
+                     VALUE_ACCESS_KEYS['o']: v1,
+                     VALUE_ACCESS_KEYS['n']: v2}
 
     return values
 
 
-def generate_diff(path_file1: str, path_file2: str, format='stylish') -> str:
+def generate_diff(path_file1: str,
+                  path_file2: str,
+                  format=DEFAULT_FORMAT_FUNCTIONS
+                  ) -> str:
 
     value_old = make_value(path_file1)
     value_new = make_value(path_file2)
@@ -76,3 +65,25 @@ def generate_diff(path_file1: str, path_file2: str, format='stylish') -> str:
     res = FORMAT_FUNCTIONS[format](values)
 
     return res
+
+
+if __name__ == '__main__':
+
+    p1 = 'second-project/python-project-50/tests/fixtures/file1.json'
+    p2 = 'second-project/python-project-50/tests/fixtures/file2.json'
+
+    value_old = make_value(p1)
+    value_new = make_value(p2)
+
+    # res = get_diff(value_old, value_new)
+
+    # print(res)
+
+    # res = stylish(res)
+    # res = plain(res)
+    # res = format_json(res)
+
+    # print(*res, sep='\n\n')
+    # print(*res, sep='\n')
+    res = generate_diff(p1, p2, 'json')
+    print(res)
