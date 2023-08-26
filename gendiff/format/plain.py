84bccbab3ec8bb1cf3d4cee096779a3fafe3d550
Alex Kositskiy <koso-96@mail.ru>
2023-08-26 17:31:36 +0300

    update plain.py

--- gendiff/format/plain.py
+++ gendiff/format/plain.py
@@ -1,81 +1,66 @@
-# String constants for handling differences.
-ADD = 'add'
-CHANGED = 'changed'
-REMOVED = 'removed'
 STATUS = 'status'
 OLD_VALUE = 'old_value'
 NEW_VALUE = 'new_value'
-FIRST_WORD = 'Property '
-
-# String constants for formatting the difference result.
-ADDED_KEY = ' was added with value: '
-CHANGED_KEY = (' was updated. From ', ' to ')
-REMOVED_KEY = ' was removed'
-
-CONSTANT_CHANGE = {'False': 'false', 'True': 'true', 'None': 'null', '0': '0'}
-HIDDEN_DICT = '[complex value]'
 
 
-def get_value_right_format(value) -> str:
-    return (HIDDEN_DICT
-            if isinstance(value, dict)
-            else f"'{value}'")
+def replace_value(value):
+    replacement_constants = {False: 'false',
+                             True: 'true',
+                             None: 'null',
+                             '0': '0',
+                             }
+    hidden_dict = '[complex value]'
 
-
-def get_lines(key, values):
-    value_keys = values.keys()
-
-    if OLD_VALUE not in value_keys:
-        new_value = values[NEW_VALUE]
-        value = get_value_right_format(new_value)
-
-        res = [[key] + [ADDED_KEY] + [value]]
-    elif NEW_VALUE not in value_keys:
-        res = [[key] + [REMOVED_KEY]]
+    if isinstance(value, dict):
+        res = hidden_dict
     else:
-        old_value = get_value_right_format(values[OLD_VALUE])
-        new_value = get_value_right_format(values[NEW_VALUE])
+        res = (replacement_constants[value]
+               if value in replacement_constants.keys()
+               else f"'{value}'")
 
-        res = [[key]
-               + [CHANGED_KEY[0]]
-               + [old_value]
-               + [CHANGED_KEY[1]]
-               + [new_value]]
     return res
 
 
-def make_plain(data: dict) -> list:
+def formats_data(data: dict, first_word="Property '") -> list:
     res = []
 
     for key, values in data.items():
-        if not isinstance(values, dict):
-            continue
+        flag = isinstance(values, dict)
 
-        value_keys = values.keys()
+        if flag and STATUS not in values.keys():
+            res += [[first_word]
+                    + [f"{key}."]
+                    + value for value in formats_data(values, '')
+                    ]
 
-        if STATUS not in value_keys:
-            for value in make_plain(values):
-                res += [[f"{key}."] + value]
-            continue
+        elif flag and values[STATUS] == 'add':
+            value = replace_value(values[NEW_VALUE])
+            res += [[first_word]
+                    + [key]
+                    + ["' was added with value: "]
+                    + [value]
+                    ]
 
-        res += get_lines(key, values)
+        elif flag and values[STATUS] == 'removed':
+            res += [[first_word] + [key] + ["' was removed"]]
+
+        elif flag and values[STATUS] == 'changed':
+            old_value = replace_value(values[OLD_VALUE])
+            new_value = replace_value(values[NEW_VALUE])
+
+            res += [[first_word] + [key]
+                    + ["' was updated. From "]
+                    + [old_value]
+                    + [' to ']
+                    + [new_value]
+                    ]
 
     return res
 
 
-def plain(data: dict) -> str:
-
-    processed_data = make_plain(data)
+def make_plain(data: dict) -> str:
+    processed_data = formats_data(data)
     lines = sorted([''.join(v) for v in processed_data])
-    lines = list(map(lambda x:
-                     ' '.join([FIRST_WORD
-                               + f"'{x.split()[0]}'"]
-                              + x.split()[1:]),
-                     lines))
-
     res = '\n'.join(lines)
-
-    for key, value in CONSTANT_CHANGE.items():
-        res = res.replace(f"'{key}'", value)
 
     return res
