da40bd7992e33a2a4038d3d06b8f0a63c6f02846
Alex Kositskiy <koso-96@mail.ru>
2023-09-06 22:23:51 +0300

    update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -10,27 +10,24 @@ P2_JSON = 'tests/fixtures/file2.json'
 P1_YAML = 'tests/fixtures/file1.yml'
 P2_YAML = 'tests/fixtures/file2.yml'
 
-with open('tests/fixtures/check_stylish.txt') as f:
-    RES_STYLISH = f.read()
+PATH_STYLISH = 'tests/fixtures/check_stylish.txt'
+PATH_PLAIN = 'tests/fixtures/check_plain.txt'
+PATH_JSON = 'tests/fixtures/check_json.txt'
+PATH_IDENTICAL_FILES = 'tests/fixtures/check_identical_files.txt'
 
-with open('tests/fixtures/check_plain.txt') as f:
-    RES_PLAIN = f.read()
-
-with open('tests/fixtures/check_json.txt') as f:
-    RES_JSON = f.read()
-
-OPTIONS = [(P1_JSON, P2_JSON, 'stylish', RES_STYLISH),
-           (P1_JSON, P2_JSON, 'plain', RES_PLAIN),
-           (P1_JSON, P2_JSON, 'json', RES_JSON),
-           (P1_YAML, P2_YAML, 'stylish', RES_STYLISH),
-           (P1_YAML, P2_YAML, 'plain', RES_PLAIN),
-           (P1_YAML, P2_YAML, 'json', RES_JSON),
-           (P1_JSON, P1_JSON, 'stylish', ''),
+OPTIONS = [(P1_JSON, P2_JSON, 'stylish', PATH_STYLISH),
+           (P1_JSON, P2_JSON, 'plain', PATH_PLAIN),
+           (P1_JSON, P2_JSON, 'json', PATH_JSON),
+           (P1_YAML, P2_YAML, 'stylish', PATH_STYLISH),
+           (P1_YAML, P2_YAML, 'plain', PATH_PLAIN),
+           (P1_YAML, P2_YAML, 'json', PATH_JSON),
+           (P1_JSON, P1_JSON, 'stylish', PATH_IDENTICAL_FILES),
            ]
 
 
-@pytest.mark.parametrize("path1, path2, format, res_comparison", OPTIONS)
-def test_generate_diff(path1, path2, format, res_comparison):
+@pytest.mark.parametrize("path1, path2, format, path_check_file", OPTIONS)
+def test_generate_diff(path1, path2, format, path_check_file):
     res = generate_diff(path1, path2, format)
 
-    assert res == res_comparison
+    with open(path_check_file) as check_file:
+        assert res == check_file.read()
